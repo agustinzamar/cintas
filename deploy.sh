1225f1da4f9e64639d77b4366b51a3710b2523b4
Agustin Zamar <agustinzamar33@gmail.com>
2023-03-14 14:15:56 -0300

    update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 git pull origin master \
 && composer install \
 && php artisan migrate \
-&& wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | bash \
 && export NVM_DIR="$HOME/.nvm"
    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" \  # This loads nvm bash_completion
